212fd28edccb8288980fea6a82017066483bd112
Luffyreals <103875830+Luffyreals@users.noreply.github.com>
2024-11-10 22:27:10 +0700

    Add files via upload

---
+++ C01002.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#define int long long
+
+const int N = 1e6+5;
+
+signed main() {
+	int t;
+	scanf("%lld", &t);
+	while(t--) {
+		int n;
+		scanf("%lld", &n);
+		printf("%lld\n", n*2);
+	}
+}
